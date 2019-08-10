07f8868919357c9f789efcce89eb81fdb26f1d68
mottetts <motechrisw@gmail.com>
2019-08-10 14:48:22 -0400

    Normalize caps of string entered and return actual title of Wiki page

--- bday_lookup.py
+++ bday_lookup.py
@@ -9,31 +9,44 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+#normalize capitalization of string for easier lookup
+def normCaps(name):
+    y = []
+    x = name.split()
+    for i in range(len(x)):
+        y.append(x[i].capitalize())
+    newname = ' '.join(y)
+    return newname
+
+#look up name as stored in first heading of page and birthday
+#return none if no birthday available
 def birthday(res):
     try:
         bs = BeautifulSoup(res.text, 'lxml')
         date = bs.find("span", class_="bday").text
+        name = bs.find("h1", class_="firstHeading").text
         ndate = datetime.strptime(date, '%Y-%m-%d')
-        return ndate.strftime("%B %d, %Y")
-    except AttributeError:
+        return name,ndate.strftime("%B %d, %Y")
+    except:
         return None
 
 print(
     '''
     Enter the name of a famous person with a Wikipedia page 
-    to look up their birthday.  Type CTRL+D to quit.
+    to look up their birthday.  Type Q to quit.
     ''')
 
 while True:
-    name = input('> ')
+    name = normCaps(input('> '))
+    if name == 'Q': break
 
     url = 'https://en.wikipedia.org/wiki/'+name
     res = requests.get(url)
     
     if res.status_code == 200:
-        bday = birthday(res)
+        wikiname,bday = birthday(res)
         if bday != None:
-            print(name, "was born on", bday + ".")
+            print(wikiname, "was born on", bday + ".")
         else:
             print("There doesn't seem to be a birthday associated with this person.")
     else:
